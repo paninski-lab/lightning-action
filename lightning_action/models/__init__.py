e4c61a56c31e2660d7a424ecfe56b7df3f7b6717
rowem5 <54791449+rowem5@users.noreply.github.com>
2026-01-15 16:34:56 -0600

    Update __init__.py

--- lightning_action/models/__init__.py
+++ lightning_action/models/__init__.py
@@ -1,6 +1,6 @@
 """Models for action segmentation."""
 
-from .backbones import RNN, DilatedTCN, TemporalMLP
+from .heads import RNN, DilatedTCN, TemporalMLP
 from .segmenter import BaseModel, Segmenter
 
 __all__ = [
