3e3c87064279fbc2ff6c7632d99789f7d13aa7e8
rowem5 <54791449+rowem5@users.noreply.github.com>
2026-01-17 10:59:11 -0600

    Update __init__.py

--- lightning_action/api/__init__.py
+++ lightning_action/api/__init__.py
@@ -1,5 +1,10 @@
+from lightning_action.api.base import BaseModelAPI, chdir
 from lightning_action.api.model import Model
+from lightning_action.api.video_model import VideoModel
 
 __all__ = [
+    'BaseModelAPI',
     'Model',
+    'VideoModel',
+    'chdir',
 ]
